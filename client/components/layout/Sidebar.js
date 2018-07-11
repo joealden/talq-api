010389b58e51f15a4ed1f0a4fdc6a25f13dd4243
Joe Alden <me@joealden.com>
2018-07-11 17:12:05 +0100

    Pull the Toolbar component out into a seperate file

--- client/components/layout/Sidebar.js
+++ client/components/layout/Sidebar.js
@@ -1,63 +1,7 @@
 import React from "react";
-import Link from "next/link";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
-import constants from "../../utils/constants";
-import SettingsIcon from "../icons/SettingsIcon";
-import NewChatIcon from "../icons/NewChatIcon";
-
-const ToolbarWrapper = styled.div`
-  background-color: white;
-  border-bottom: ${constants.border};
-  height: ${constants.headerHeight};
-  padding: 10px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  a {
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  h1 {
-    font-size: 20px;
-    font-weight: normal;
-  }
-`;
-
-const iconStyles = css`
-  color: ${constants.color};
-  height: 46px;
-  width: 46px;
-  stroke-width: 1.2px;
-  padding: 6px;
-`;
-
-const StyledSettingsIcon = styled(SettingsIcon)`
-  ${iconStyles};
-`;
-const StyledNewChatIcon = styled(NewChatIcon)`
-  ${iconStyles};
-`;
-
-const Toolbar = () => (
-  <ToolbarWrapper>
-    <Link prefetch href="/settings">
-      <a>
-        <StyledSettingsIcon />
-      </a>
-    </Link>
-    <h1>Talq</h1>
-    <Link prefetch href="/new">
-      <a>
-        <StyledNewChatIcon />
-      </a>
-    </Link>
-  </ToolbarWrapper>
-);
+import Toolbar from "./Toolbar";
 
 const SidebarWrapper = styled.div`
   grid-area: "sidebar";
