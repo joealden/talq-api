cf15940ff833f595aca0a932ffbe24b7e2103786
Joe Alden <me@joealden.com>
2018-07-15 16:27:14 +0100

    Implement resolvers that return dummy data

--- api/src/index.js
+++ api/src/index.js
@@ -4,42 +4,112 @@ const { Prisma } = require("prisma-binding");
 const resolvers = {
   Query: {
     userFriends: (_, args, context, info) => {
-      // ...
+      return [
+        {
+          id: "cjjlmquwscq4m0b22qc5tqfwh",
+          email: "test@test.com",
+          firstName: "first",
+          lastName: "last",
+          friends: []
+        }
+      ];
     },
     userName: (_, args, context, info) => {
-      // ...
+      return {
+        firstName: "first",
+        lastName: "last"
+      };
     },
     chat: (_, args, context, info) => {
-      // ...
+      return {
+        id: "cjjlmquwscq4m0b22qc5tqfwh",
+        title: "test title",
+        members: [],
+        messages: []
+      };
     }
   },
   Mutation: {
     signup: (_, args, context, info) => {
-      // ...
+      return {
+        id: "cjjlmquwscq4m0b22qc5tqfwh",
+        email: "test@test.com",
+        firstName: "first",
+        lastName: "last",
+        friends: []
+      };
     },
     signin: (_, args, context, info) => {
-      // ...
+      return {
+        id: "cjjlmquwscq4m0b22qc5tqfwh",
+        email: "test@test.com",
+        firstName: "first",
+        lastName: "last",
+        friends: []
+      };
     },
     addFriend: (_, args, context, info) => {
-      // ...
+      return {
+        id: "cjjlmquwscq4m0b22qc5tqfwh",
+        email: "test@test.com",
+        firstName: "first",
+        lastName: "last",
+        friends: []
+      };
     },
     changeUserName: (_, args, context, info) => {
-      // ...
+      return {
+        id: "cjjlmquwscq4m0b22qc5tqfwh",
+        email: "test@test.com",
+        firstName: "first",
+        lastName: "last",
+        friends: []
+      };
     },
     changeUserEmail: (_, args, context, info) => {
-      // ...
+      return {
+        id: "cjjlmquwscq4m0b22qc5tqfwh",
+        email: "test@test.com",
+        firstName: "first",
+        lastName: "last",
+        friends: []
+      };
     },
     startChat: (_, args, context, info) => {
-      // ...
+      return {
+        id: "cjjlmquwscq4m0b22qc5tqfwh",
+        title: "test title",
+        members: [],
+        messages: []
+      };
     },
     updateChatTitle: (_, args, context, info) => {
-      // ...
+      return {
+        id: "cjjlmquwscq4m0b22qc5tqfwh",
+        title: "test title",
+        members: [],
+        messages: []
+      };
     },
     addMembersToChat: (_, args, context, info) => {
-      // ...
+      return {
+        id: "cjjlmquwscq4m0b22qc5tqfwh",
+        title: "test title",
+        members: [],
+        messages: []
+      };
     },
     sendMessageToChat: (_, args, context, info) => {
-      // ...
+      return {
+        id: "cjjlmquwscq4m0b22qc5tqfwh",
+        author: {
+          id: "cjjlmquwscq4m0b22qc5tqfwh",
+          email: "test@test.com",
+          firstName: "first",
+          lastName: "last",
+          friends: []
+        }
+      };
     }
   }
 };
