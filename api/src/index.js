96545135cddfd6c76782161778d64f71fddd44a8
Joe Alden <me@joealden.com>
2018-07-15 16:48:18 +0100

    Disable resolverType warning and add a comment about it

--- api/src/index.js
+++ api/src/index.js
@@ -72,6 +72,18 @@ const resolvers = {
 const server = new GraphQLServer({
   typeDefs: "src/schema.graphql",
   resolvers,
+  /*
+   * Hide resolveType warning on Node type from showing in
+   * in the console when booting the server. This warning
+   * occurs because of a disparity between the resolvers
+   * exposed by the Prisma service and the application server.
+   * 
+   * Refer to the following github issue for more information:
+   * https://github.com/prismagraphql/prisma/issues/2225
+   */
+  resolverValidationOptions: {
+    requireResolversForResolveType: false
+  },
   context: req => ({
     ...req,
     prisma: new Prisma({
