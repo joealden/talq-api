5aa89638820f35c2612cdabd64f1e9e82a467513
Joe Alden <me@joealden.com>
2018-08-05 18:52:38 +0100

    Add removeFriend mutation

--- src/resolvers/Mutation/user.ts
+++ src/resolvers/Mutation/user.ts
@@ -27,6 +27,32 @@ const userMutations = {
     );
   },
 
+  removeFriend: (_, args, context: Context, info) => {
+    /* 
+     * TODO: Check needs to be made to ensure that the 
+     * username is valid (a user exists with the username
+     * provided)
+     */
+
+    const userId = getUserId(context);
+
+    return context.prisma.mutation.updateUser(
+      {
+        where: {
+          id: userId
+        },
+        data: {
+          friends: {
+            disconnect: {
+              username: args.username
+            }
+          }
+        }
+      },
+      info
+    );
+  },
+
   changeNames: (_, args, context: Context, info) => {
     const userId = getUserId(context);
 
