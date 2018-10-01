939a3f4e935ca9eb35b776074f72eb3605dad69f
Joe Alden <me@joealden.com>
2018-10-01 22:29:30 +0100

    Update schema and resolvers to match new frontend changes

--- src/resolvers/Subscription.ts
+++ src/resolvers/Subscription.ts
@@ -1,43 +1,6 @@
-import { checkAuth, getUserId, Context } from "./utils";
+import { getUserId, Context } from "./utils";
 
 const Subscription = {
-  newChatMessage: {
-    subscribe: (_, args, context: Context, _info) => {
-      checkAuth(context);
-
-      const selectionSet = `{ 
-        node {
-          id 
-          author {
-            username
-            firstName
-            lastName
-          }
-          createdAt
-          content
-        }
-      }`;
-
-      return context.prisma.subscription.message(
-        {
-          /* Look into use of AND (this is possibly wrong) */
-          where: {
-            mutation_in: ["CREATED"],
-            node: {
-              chat: {
-                id: args.chatId
-              }
-            }
-          }
-        },
-        selectionSet
-      );
-    },
-    resolve: (payload, _args, _context, _info) => {
-      return payload ? payload.message.node : payload;
-    }
-  },
-
   updatedChat: {
     subscribe: (_, _args, context: Context, _info) => {
       const userId = getUserId(context);
